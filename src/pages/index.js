b8704056ba48a970d481eff66010c02d8be1018a
nomisatti <noumanatiqsatti.92@gmail.com>
2020-11-15 19:17:44 +0500

    remove item-url from button

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <button className="snipcart-add-item"
         data-item-id="starry-night"
         data-item-price="79.99"
-        data-item-url="/paintings/starry-night"
+        data-item-url="/"
         data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
         data-item-image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
         data-item-name="The Starry Night">
